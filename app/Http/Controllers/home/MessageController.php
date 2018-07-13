d72ed9758b2a16433afa5e58c56f563a945e0812
yangfeng0109 <m18237676730@163.com>
2018-07-13 11:52:56 +0800

    添加了留言

---
+++ app/Http/Controllers/home/MessageController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers\home;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use DB;
+
+class MessageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function Index()
+    {
+        //
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function getCreate()
+    {
+        //
+        $message = DB::table('message as m')
+            -> join('user as u','m.uid','=','u.id')
+            -> select('m.content','m.created_at','u.uname','u.avatar')
+            ->get();
+        // dump($message);
+
+        return view('home.message.index',['message'=>$message]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function postStore(Request $request)
+    {
+        //
+        $message = $request -> except(['_token']);
+        $message['created_at'] = date('Y-m-d H:i:s',time());
+        $res = DB::table('message')
+            ->insert(['content'=>$message['content'],
+                'created_at'=>$message['created_at']]);
+        dump($message);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
